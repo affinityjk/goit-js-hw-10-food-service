d0f832623a368ff143ca17267bfc6a64ed3b8873
Julia Kutsyn <yuliyaflm@gmail.com>
2021-04-27 16:25:20 +0300

    adds theme switch and alteres css file to scss

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,4 +1,38 @@
-export const Theme = {
+const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+const bodyClassRef = document.querySelector('body');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+function setLightTheme() {
+  bodyClassRef.classList.remove(Theme.DARK);
+  bodyClassRef.classList.add(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function setDarkTheme() {
+  bodyClassRef.classList.remove(Theme.LIGHT);
+  bodyClassRef.classList.add(Theme.DARK);
+  localStorage.setItem('theme', Theme.DARK);
+}
+
+function checkCurrentTheme(currentTheme) {
+  if (currentTheme === Theme.DARK) {
+    setDarkTheme();
+    checkboxRef.checked = true;
+  }
+}
+
+function onCheckboxChange({ target }) {
+  if (target.checked) {
+    setDarkTheme();
+  } else {
+    setLightTheme();
+  }
+}
+
+checkboxRef.addEventListener('change', onCheckboxChange);
+checkCurrentTheme(currentTheme);
